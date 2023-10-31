c3da37bc9c413375d44d7bf02dc121d06f2afdec
Luiz-Dias01 <142491324+Luiz-Dias01@users.noreply.github.com>
2023-10-31 03:19:33 -0300

    create naturalmente

---
+++ naturalmente/site/src/pages/landingPage/index.js
@@ -0,0 +1,12 @@
+import './index.scss';
+
+export default function Home() {
+  return (
+    <div className="page-home">
+      <header>
+        <h1>hello wold</h1>
+      </header>
+    </div>
+  );
+}
+
